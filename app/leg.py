015f3cf16526ba09c75f1933365592d4f747b594
Alhassane <ali75009@gmail.com>
2023-04-27 22:27:16 +0200

    pytest

--- app/leg.py
+++
@@ -1,5 +0,0 @@
-main_list = [11, 21, 19, 18, 46]
-check_elements = [11, 19, 46]
-
-k = all([item in check_elements  for item in main_list])
-print(k)
--- app/tests/test.py
+++ app/tests/test.py
@@ -1,13 +1,25 @@
 import pytest
-from app import training_features, loan_scoring_classifier, raw_data
+from app import training_features, raw_data
 
 
 def test_training_features():
-    assert len(training_features)==797
+    assert len(training_features) == 797
+
 
 def test_personal_infos_features():
-    infos_features = ["AMT_INCOME_TOTAL", "AMT_CREDIT","AMT_ANNUITY", "DAYS_BIRTH", "CODE_GENDER", "NAME_FAMILY_STATUS",
-                      "NAME_EDUCATION_TYPE", "DAYS_EMPLOYED", "NAME_INCOME_TYPE", "AMT_INCOME_TOTAL","NAME_CONTRACT_TYPE",]
+    infos_features = [
+        "AMT_INCOME_TOTAL",
+        "AMT_CREDIT",
+        "AMT_ANNUITY",
+        "DAYS_BIRTH",
+        "CODE_GENDER",
+        "NAME_FAMILY_STATUS",
+        "NAME_EDUCATION_TYPE",
+        "DAYS_EMPLOYED",
+        "NAME_INCOME_TYPE",
+        "AMT_INCOME_TOTAL",
+        "NAME_CONTRACT_TYPE",
+    ]
 
-    compare = all([item in raw_data.columns   for item in infos_features])
+    compare = all([item in raw_data.columns for item in infos_features])
     assert compare == True
